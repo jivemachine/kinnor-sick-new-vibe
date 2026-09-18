d23298edcd3037a65c26ef1186d1a499ea6f7fe0
Jeremy <the.jeremy.cobb@gmail.com>
2026-09-18 02:55:05 -0500

    update: add MenuFrame type for menu item structure and clarity

--- resources/js/types/kinnor.ts
+++ resources/js/types/kinnor.ts
@@ -1,1 +1,8 @@
 export type BusinessHour = readonly [day: string, hours: string];
+
+export type MenuFrame = {
+    number: string;
+    label: string;
+    note: string;
+    availability: string;
+};
